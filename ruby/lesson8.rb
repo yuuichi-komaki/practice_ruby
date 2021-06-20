d12213c25de1b2ab7dca52ceaf049cbff9c3ba80
yuuichi-komaki <yuuichi.k.1109@gmail.com>
2021-06-20 04:44:39 +0000

    練習

--- ruby/lesson8.rb
+++ ruby/lesson8.rb
@@ -1,10 +1,25 @@
 
-numbers = *1..10
-total = 0
-# puts numbers
+# numbers = *1..10
+# total = 0
+# # puts numbers
 
-numbers.each do |i|
-  total += i
-end 
+# numbers.each do |i|
+#   total += i
+# end 
+
+# puts total
+
+# total = 1
+# index = *1..30
+ 
+#  index.each do |i|
+#    total *= i
+#  end 
+ 
+#  p total
+ 
+number = gets.to_i
+
+total = number * 2 
 
 puts total
